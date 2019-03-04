93edb6b276df3bce29de41f5455a3f82f3790a76
Nickita Solovei <https://github.com/NickitaSolovei>
2019-03-04 13:12:06 +0200

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let maxSimpleNum = base;
+  let arrTime = []; 
+  let getMaxSimpleNum = (simpleNum) => {
+    let simpleNumTime = simpleNum;
+    let arr = [];
+    for (let i = 2; i <= simpleNum ; i++){
+      let balance = simpleNumTime % i;
+      if (balance === 0){
+        if (arr[i] === undefined) {
+          arr[i] = 0;
+        }
+        arr[i]++;
+        simpleNumTime /= i;
+        i--;
+      }
+    }
+    return arr;
+  }
+  arrTime = getMaxSimpleNum(maxSimpleNum);
+  let arrNew = [];
+  for (let i = 0; i < arrTime.length; i++) {
+    if (arrTime[i] !== undefined) {
+      arrNew.push([i, arrTime[i]])
+    }
+  }
+  let divideBase = (n, maxSimpleNum) => {
+    let answer = 0;
+    let newN = n / maxSimpleNum;
+    if (newN >= 1){
+      newN = Math.floor(newN);
+      answer += newN;
+      answer += divideBase(newN, maxSimpleNum);
+    }
+    return answer;
+  }
+  for (let i = 0; i < arrNew.length; i++) {
+    arrNew[i][2] = Math.floor(divideBase(number, arrNew[i][0]) / (arrNew[i][1]));
+  }
+  arrNew.sort((a, b) => a[2] - b[2]);
+  return arrNew[0][2];
 }
